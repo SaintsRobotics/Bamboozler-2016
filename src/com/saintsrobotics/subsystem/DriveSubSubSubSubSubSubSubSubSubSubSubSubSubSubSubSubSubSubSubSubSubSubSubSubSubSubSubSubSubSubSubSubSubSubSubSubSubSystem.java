88fa13c574f891636452f4d07a7a5eff49f60d5a
Driver Station <fellfromatree@gmail.com>
2016-03-07 18:13:18 -0800

    stipd joke removed

--- src/com/saintsrobotics/subsystem/DriveSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSystem.java
+++
@@ -1,5 +0,0 @@
-package com.saintsrobotics.subsystem;
-
-public class DriveSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSubSystem {
-	//So, there's no other purpose to this class than a stupid joke I made.
-}
